a06a361b2df15fced4d9b2994cd101706fdef16c
LucasStoddard <lucasjohnstoddard@hotmail.com>
2025-11-09 17:55:16 -0700

    Having plenty of fun

--- src/database/database.js
+++ src/database/database.js
@@ -341,7 +341,7 @@ class DB {
   }
 
   async query(connection, sql, params) {
-    logger.dbLogger(sql);
+    // logger.dbLogger(sql);
     const [results] = await connection.execute(sql, params);
     return results;
   }
